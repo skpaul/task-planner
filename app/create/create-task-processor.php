a8d8ab6d27e2cddfaa6a1729dc38bec20b59c914
Saumitra Paul <skpaul@gmail.com>
2022-08-08 17:06:06 +0600

    SMS and mail notification added in new task.

--- app/create/create-task-processor.php
+++ app/create/create-task-processor.php
@@ -2,6 +2,15 @@
 
     declare(strict_types=1);
 
+    //Import PHPMailer classes into the global namespace
+    //These must be at the top of your script, not inside a function
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
+    //Load Composer's autoloader
+    require '../../vendor/autoload.php';
+
     #region Import libraries
     require_once("../../Required.php");
     Required::Logger()
@@ -72,9 +81,38 @@
         $task["taskStatusId"] = 1;
         $task["createdOn"] =  $now;
 
-        
+        $developer = $db->selectSingle("SELECT * FROM developers WHERE developerId={$task["assignedTo"]}");
         $id = $db->insert("tasks", $task);
-       
+        $text = urlencode("You have got a new task. Please visit task-planner");
+        $number = $developer->mobile;
+        $smsresult = file_get_contents("http://66.45.237.70/api.php?username=skpaul&password=New@2022$2022&number=$number&message=$text");
+    
+
+        //Create an instance; passing `true` enables exceptions
+        $mail = new PHPMailer(true);
+        //Server settings
+        // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'winbip.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'info@winbip.com';                     //SMTP username
+        $mail->Password   = 'ParkAvenue$1978';                               //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+        //Recipients
+        $mail->setFrom('info@winbip.com', 'Winbip Solutions');
+        $mail->addReplyTo('info@winbip.com', 'Winbip Solutions');
+        $mail->addAddress($developer->email1);
+        $mail->addCC($developer->email2);
+
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = 'New Task';
+        $mail->Body    = 'You have been assigned a new task. Please visit task-planner';
+        // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        $mail->send();
 
     } catch (\Exception $exp) {
         $logger->createLog($exp->getMessage());
